75018f59efd4dd9eb770c0331edd0e6a49419719
0xAA55 <weixiaobao213@qq.com>
2025-04-29 14:14:03 +0800

    添加 `i24` 和 `u24` 自定义类型。

---
+++ src/mod_i24.rs
@@ -0,0 +1,254 @@
+#![allow(dead_code)]
+
+use std::ops::{Add, Sub, Mul, Div, AddAssign, SubAssign, MulAssign, DivAssign};
+use std::ops::{BitAnd, BitOr, BitXor, Shl, Shr, BitAndAssign, BitOrAssign, BitXorAssign, ShlAssign, ShrAssign};
+use std::ops::{Rem, RemAssign};
+use std::ops::{Neg};
+
+/// * The tuple struct is little-endian
+#[derive(Debug, Clone, Copy)]
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct i24(pub u8, pub u8, pub u8);
+
+impl i24{
+    #[inline(always)]
+    fn from_le_bytes(bytes: &[u8]) -> Self {
+        Self(bytes[0], bytes[1], bytes[2])
+    }
+    #[inline(always)]
+    fn from_be_bytes(bytes: &[u8]) -> Self {
+        Self(bytes[2], bytes[1], bytes[0])
+    }
+    #[inline(always)]
+    fn to_le_bytes(self) -> [u8; 3] {
+        [self.0, self.1, self.2]
+    }
+    #[inline(always)]
+    fn to_be_bytes(self) -> [u8; 3] {
+        [self.2, self.1, self.0]
+    }
+    #[inline(always)]
+    fn as_i64(&self) -> i64 {
+        i64::from_le_bytes([self.0, self.1, self.2, 0, 0, 0, 0, 0])
+    }
+    #[inline(always)]
+    fn as_i32(&self) -> i32 {
+        i32::from_le_bytes([self.0, self.1, self.2, 0])
+    }
+    #[inline(always)]
+    fn as_i16(&self) -> i16 {
+        i16::from_le_bytes([self.0, self.1])
+    }
+    #[inline(always)]
+    fn as_i8(&self) -> i8 {
+        self.0 as i8
+    }
+    #[inline(always)]
+    fn as_u64(&self) -> u64 {
+        u64::from_le_bytes([self.0, self.1, self.2, 0, 0, 0, 0, 0])
+    }
+    #[inline(always)]
+    fn as_u32(&self) -> u32 {
+        u32::from_le_bytes([self.0, self.1, self.2, 0])
+    }
+    #[inline(always)]
+    fn as_u16(&self) -> u16 {
+        u16::from_le_bytes([self.0, self.1])
+    }
+    #[inline(always)]
+    fn as_u8(&self) -> u8 {
+        self.0
+    }
+}
+
+impl From<i8> for i24 {
+    #[inline(always)]
+	fn from(v: i8) -> Self {
+		Self::from(v as i32)
+	}
+}
+impl From<i16> for i24 {
+    #[inline(always)]
+	fn from(v: i16) -> Self {
+		Self::from(v as i32)
+	}
+}
+impl From<i32> for i24 {
+    #[inline(always)]
+	fn from(v: i32) -> Self {
+		let bytes = v.to_le_bytes();
+		Self(bytes[0], bytes[1], bytes[2])
+	}
+}
+impl From<i64> for i24 {
+    #[inline(always)]
+	fn from(v: i64) -> Self {
+		Self::from(v as i32)
+	}
+}
+impl From<u8> for i24 {
+    #[inline(always)]
+	fn from(v: u8) -> Self {
+		Self::from(v as u32)
+	}
+}
+impl From<u16> for i24 {
+    #[inline(always)]
+	fn from(v: u16) -> Self {
+		Self::from(v as u32)
+	}
+}
+impl From<u32> for i24 {
+    #[inline(always)]
+	fn from(v: u32) -> Self {
+		let bytes = v.to_le_bytes();
+		Self(bytes[0], bytes[1], bytes[2])
+	}
+}
+impl From<u64> for i24 {
+    #[inline(always)]
+	fn from(v: u64) -> Self {
+		Self::from(v as u32)
+	}
+}
+
+impl Add for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::from(self.as_i32() + rhs.as_i32())
+    }
+}
+impl Sub for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::from(self.as_i32() - rhs.as_i32())
+    }
+}
+impl Mul for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::from(self.as_i32() * rhs.as_i32())
+    }
+}
+impl Div for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn div(self, rhs: Self) -> Self::Output {
+        Self::from(self.as_i32() / rhs.as_i32())
+    }
+}
+impl AddAssign for i24 {
+    #[inline(always)]
+    fn add_assign(&mut self, rhs: Self) {
+        *self = self.add(rhs);
+    }
+}
+impl SubAssign for i24 {
+    #[inline(always)]
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = self.sub(rhs);
+    }
+}
+impl MulAssign for i24 {
+    #[inline(always)]
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = self.mul(rhs);
+    }
+}
+impl DivAssign for i24 {
+    #[inline(always)]
+    fn div_assign(&mut self, rhs: Self) {
+        *self = self.div(rhs);
+    }
+}
+impl BitAnd for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Self(self.0 & rhs.0, self.1 & rhs.1, self.2 & rhs.2)
+    }
+}
+impl BitOr for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn bitor(self, rhs: Self) -> Self::Output {
+        Self(self.0 | rhs.0, self.1 | rhs.1, self.2 | rhs.2)
+    }
+}
+impl BitXor for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        Self(self.0 ^ rhs.0, self.1 ^ rhs.1, self.2 ^ rhs.2)
+    }
+}
+impl Shl for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn shl(self, rhs: Self) -> Self::Output {
+        Self::from(self.as_i32() << rhs.as_i32())
+    }
+}
+impl Shr for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn shr(self, rhs: Self) -> Self::Output {
+        Self::from(self.as_i32() >> rhs.as_i32())
+    }
+}
+impl BitAndAssign for i24 {
+    #[inline(always)]
+    fn bitand_assign(&mut self, rhs: Self) {
+        *self = self.bitand(rhs);
+    }
+}
+impl BitOrAssign for i24 {
+    #[inline(always)]
+    fn bitor_assign(&mut self, rhs: Self) {
+        *self = self.bitor(rhs);
+    }
+}
+impl BitXorAssign for i24 {
+    #[inline(always)]
+    fn bitxor_assign(&mut self, rhs: Self) {
+        *self = self.bitxor(rhs);
+    }
+}
+impl ShlAssign for i24 {
+    #[inline(always)]
+    fn shl_assign(&mut self, rhs: Self) {
+        *self = self.shl(rhs);
+    }
+}
+impl ShrAssign for i24 {
+    #[inline(always)]
+    fn shr_assign(&mut self, rhs: Self) {
+        *self = self.shr(rhs);
+    }
+}
+impl Rem for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn rem(self, rhs: Self) -> Self::Output {
+        Self::from(self.as_i32() % rhs.as_i32())
+    }
+}
+impl RemAssign for i24 {
+    #[inline(always)]
+    fn rem_assign(&mut self, rhs: Self) {
+        *self = self.rem(rhs);
+    }
+}
+impl Neg for i24 {
+    type Output = Self;
+    #[inline(always)]
+    fn neg(self) -> Self::Output {
+        let ret = Self(!self.0, !self.1, !self.2);
+        ret.add(Self(1, 0, 0))
+    }
+}
+
