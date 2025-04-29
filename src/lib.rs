d2968281a23a05e8e8bc647faa3b3678db5f1b8c
0xAA55 <weixiaobao213@qq.com>
2025-04-29 21:36:19 +0800

    使 `Read` 和 `Write` 用泛型并且增加 `?Sized` 以允许传入对象。

--- src/lib.rs
+++ src/lib.rs
@@ -873,10 +873,10 @@ pub trait SampleType: Numeric {
     fn is_float() -> bool;
     fn to_signed(&self) -> Self::Signed;
     fn to_unsigned(&self) -> Self::Unsigned;
-    fn read_le(r: &mut impl Read) -> Result<Self, Error>;
-    fn read_be(r: &mut impl Read) -> Result<Self, Error>;
-    fn write_le(&self, w: &mut impl Write) -> Result<(), Error>;
-    fn write_be(&self, w: &mut impl Write) -> Result<(), Error>;
+    fn read_le<T>(r: &mut T) -> Result<Self, Error> where T: Read + ?Sized;
+    fn read_be<T>(r: &mut T) -> Result<Self, Error> where T: Read + ?Sized;
+    fn write_le<T>(&self, w: &mut T) -> Result<(), Error> where T: Write + ?Sized;
+    fn write_be<T>(&self, w: &mut T) -> Result<(), Error> where T: Write + ?Sized;
 }
 
 pub trait SampleFrom: Numeric {
@@ -1033,23 +1033,27 @@ macro_rules! impl_sample_type {
                 to_unsigned!($tp, *self)
             }
             #[inline(always)]
-            fn read_le(r: &mut impl Read) -> Result<Self, Error> {
+            fn read_le<T>(r: &mut T) -> Result<Self, Error>
+            where T: Read + ?Sized {
                 let mut buf = [0u8; sizeof!($tp)];
                 r.read_exact(&mut buf)?;
                 Ok(Self::from_le_bytes(buf))
             }
             #[inline(always)]
-            fn read_be(r: &mut impl Read) -> Result<Self, Error> {
+            fn read_be<T>(r: &mut T) -> Result<Self, Error>
+            where T: Read + ?Sized {
                 let mut buf = [0u8; sizeof!($tp)];
                 r.read_exact(&mut buf)?;
                 Ok(Self::from_be_bytes(buf))
             }
             #[inline(always)]
-            fn write_le(&self, w: &mut impl Write) -> Result<(), Error> {
+            fn write_le<T>(&self, w: &mut T) -> Result<(), Error>
+            where T: Write + ?Sized {
                 w.write_all(&self.to_le_bytes())
             }
             #[inline(always)]
-            fn write_be(&self, w: &mut impl Write) -> Result<(), Error> {
+            fn write_be<T>(&self, w: &mut T) -> Result<(), Error>
+            where T: Write + ?Sized {
                 w.write_all(&self.to_be_bytes())
             }
         }
