40b2c20129d2d63c9aa97db25a9c79d94848a68c
0xAA55 <weixiaobao213@qq.com>
2025-04-29 14:14:20 +0800

    准备实现 `SampleType`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,98 @@
+#![allow(dead_code)]
 
+pub mod mod_i24;
+pub mod mod_u24;
 
+pub use mod_i24::*;
+pub use mod_u24::*;
 
+use std::{any::type_name, io::{Read, Write, Error}, mem::size_of, fmt::Debug, clone::Clone};
+use std::ops::{Add, Sub, Mul, Div, AddAssign, SubAssign, MulAssign, DivAssign};
+use std::ops::{BitAnd, BitOr, BitXor, Shl, Shr, BitAndAssign, BitOrAssign, BitXorAssign, ShlAssign, ShrAssign};
+use std::ops::{Rem, RemAssign};
+use std::ops::{Neg};
+
+pub trait SampleType:
+Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Div<Output = Self> +
+AddAssign + SubAssign + MulAssign + DivAssign + Neg +
+Debug + Sized + Clone + Copy + 'static {
+    type Longer;
+    type Shorter;
+    type Signed;
+    type Unsigned;
+    fn new() -> Self;
+    fn from(v: impl SampleType) -> Self;
+    fn average(s1: Self, s2: Self) -> Self;
+    fn average_arr(arr: &[Self]) -> Self;
+    fn to_i8 (&self) -> i8;
+    fn to_i16(&self) -> i16;
+    fn to_i24(&self) -> i24;
+    fn to_i32(&self) -> i32;
+    fn to_i64(&self) -> i64;
+    fn to_u8 (&self) -> u8;
+    fn to_u16(&self) -> u16;
+    fn to_u24(&self) -> u24;
+    fn to_u32(&self) -> u32;
+    fn to_u64(&self) -> u64;
+    fn to_f32(&self) -> f32;
+    fn to_f64(&self) -> f64;
+    fn as_i8 (&self) -> i8;
+    fn as_i16(&self) -> i16;
+    fn as_i24(&self) -> i24;
+    fn as_i32(&self) -> i32;
+    fn as_i64(&self) -> i64;
+    fn as_u8 (&self) -> u8;
+    fn as_u16(&self) -> u16;
+    fn as_u24(&self) -> u24;
+    fn as_u32(&self) -> u32;
+    fn as_u64(&self) -> u64;
+    fn as_f32(&self) -> f32;
+    fn as_f64(&self) -> f64;
+    fn to_longer(&self) -> Self::Longer;
+    fn to_shorter(&self) -> Self::Shorter;
+    fn is_signed(&self) -> bool;
+    fn is_unsigned(&self) -> bool;
+    fn is_integer(&self) -> bool;
+    fn is_float(&self) -> bool;
+    fn to_signed(&self) -> Self::Signed;
+    fn to_unsigned(&self) -> Self::Unsigned {
+        panic!("The type `{}` can't be turned to an unsigned type.", type_name::<Self>());
+    }
+    fn read_le<T>(r: &mut T) -> Result<Self, Error> where T: Read + ?Sized;
+    fn read_be<T>(r: &mut T) -> Result<Self, Error> where T: Read + ?Sized;
+    fn write_le<T>(&self, w: &mut T) -> Result<(), Error> where T: Write + ?Sized;
+    fn write_be<T>(&self, w: &mut T) -> Result<(), Error> where T: Write + ?Sized;
+}
+
+pub trait SampleFrom: Debug + Sized + Clone + Copy + 'static {
+    fn to(s: impl SampleType) -> Self;
+}
+impl SampleFrom for i8  {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_i8()  }}
+impl SampleFrom for i16 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_i16() }}
+impl SampleFrom for i24 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_i24() }}
+impl SampleFrom for i32 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_i32() }}
+impl SampleFrom for i64 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_i64() }}
+impl SampleFrom for u8  {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_u8()  }}
+impl SampleFrom for u16 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_u16() }}
+impl SampleFrom for u24 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_u24() }}
+impl SampleFrom for u32 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_u32() }}
+impl SampleFrom for u64 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_u64() }}
+impl SampleFrom for f32 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_f32() }}
+impl SampleFrom for f64 {#[inline(always)] fn to(s: impl SampleType) -> Self { s.to_f64() }}
+
+pub trait SampleTypeInteger:
+SampleType +
+BitAnd<Output = Self> +
+BitOr<Output = Self> +
+BitXor<Output = Self> +
+Shl<Output = Self> +
+Shr<Output = Self> +
+BitAndAssign +
+BitOrAssign +
+BitXorAssign +
+ShlAssign +
+ShrAssign +
+Rem<Output = Self> +
+RemAssign {}
 
 
